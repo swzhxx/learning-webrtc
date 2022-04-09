2d7199e33a4a70e8aec9b1d633c5a05e8ca2f3c0
swzhxx <549909896@qq.com>
2022-04-09 22:36:03 +0800

    feat(player): flv解析

--- self-player/src/utils/decoder.ts
+++ self-player/src/utils/decoder.ts
@@ -17,6 +17,64 @@ enum FlvTagType {
   Unknown = 0
 }
 
+enum FrameType {
+  KeyFrame = 1,
+  InterFrame = 2,
+  DisposableFrame = 3,
+  GeneratedKeyFrame = 4,
+  VideoInfoFrame = 5
+}
+
+enum EncodeType {
+  JPEG = 1,
+  H263 = 2,
+  ScreenVideo = 3,
+  On2VP6 = 4,
+  On2VP6WithAlphaChannel = 5,
+  ScreenVideoVersion2 = 6,
+  AVC = 7
+}
+
+enum AVCPacketType {
+  AVCDecoderConfigurationRecord = 0,
+  AVC_NALU = 1,
+  AVC_END_OF_SEQUENCE = 3
+}
+
+enum AudioSoundFormat {
+  LINEAR_PCM_PLATFORM_ENDIAN = 0,
+  ADPCM = 1,
+  MP3 = 2,
+  LINEAR_PCM_LITTLE_ENDIAN = 3,
+  NELLYMOSER_16KHZ_MONO = 4,
+  NELLYMOSER_8KHZ_MONO = 5,
+  NELLYMOSER = 6,
+  G711_ALAW_LOGARITHMIC_PCM = 7,
+  G711_MULAW_LOGARITHMIC_PCM = 8,
+  RESERVED = 9,
+  AAC = 10,
+  SPEEX = 11,
+  MP3_8KHZ = 14,
+  DEVICE_SPECIFIC_SOUND = 15
+}
+
+enum AudioSamplingRate {
+  SAMPLING_5_POINT_5_KHZ = 0,
+  SAMPLING_11_KHZ = 1,
+  SAMPLING_22_KHZ = 2,
+  SAMPLING_44_KHZ = 3
+}
+
+enum AudioSamplingPrecision {
+  SND_8_BIT = 0,
+  SND_16_BIT = 1
+}
+
+enum AudioSoundTrack {
+  SND_MONO = 0,
+  SND_STEREO = 1
+}
+
 const number2TagType = (num: number): FlvTagType => {
   switch (num) {
     case FlvTagType.Audio: {
@@ -72,17 +130,17 @@ class FlvHeader implements FromByteBuffer, FormatPrint {
     self.signautre = `${char1}${char2}${char3}`
     self.version = version
     self.flags = flags
-    self.dataOffset = buffer.readInt32()
+    self.dataOffset = buffer.readUint32()
     return self
   }
 }
 
 class Tag implements FromByteBuffer {
   tagHeader: TagHeader
-  tagData: Array<number>
+  tagData: VideoData | AudioData | ScriptData | null
   constructor() {
     this.tagHeader = new TagHeader()
-    this.tagData = []
+    this.tagData = null
   }
   fromByteBuffer(buffer: ByteBuffer): this {
     throw new Error('Method not implemented.')
@@ -90,13 +148,30 @@ class Tag implements FromByteBuffer {
   static fromByteBuffer(buffer: ByteBuffer): Tag {
     // throw new Error('Method not implemented.')
     let tagHeader = TagHeader.fromByteBuffer(buffer)
-    let tagData = buffer.readBytes(tagHeader.dataSize)
+
+    let data = buffer.readBytes(tagHeader.dataSize)
+
+    data = data.slice()
     let tag = new Tag()
+    let tagData = null
     tag.tagHeader = tagHeader
-    if (tag.tagHeader.type == FlvTagType.ScriptData) {
-      let scriptData = ScriptData.fromByteBuffer(tagData)
+
+    switch (tag.tagHeader.type) {
+      case FlvTagType.ScriptData: {
+        tagData = ScriptData.fromByteBuffer(data)
+        break
+      }
+      case FlvTagType.Video: {
+        tagData = VideoData.fromByteBuffer(data)
+        break
+      }
+      case FlvTagType.Audio: {
+        tagData = AudioData.fromByteBuffer(data)
+        break
+      }
     }
-    // tag.tagData = tagData
+    tagData?.formatPrint()
+    tag.tagData = tagData
     return tag
   }
 }
@@ -132,10 +207,12 @@ class TagHeader implements FromByteBuffer {
   }
   static fromByteBuffer(buffer: ByteBuffer): TagHeader {
     let type = number2TagType(buffer.readByte())
-    let dataSize = (buffer.readInt16() << 8) + buffer.readInt8()
-    let timeStamp = (buffer.readInt16() << 8) + buffer.readInt8()
-    let timeStampEx = buffer.readInt8()
-    let streamId = (buffer.readInt16() << 8) + buffer.readInt8()
+    let a = buffer.readUint16()
+    let b = buffer.readUint8()
+    let dataSize = (a << 8) + b
+    let timeStamp = (buffer.readUint16() << 8) + buffer.readUint8()
+    let timeStampEx = buffer.readUint8()
+    let streamId = (buffer.readUint16() << 8) + buffer.readUint8()
 
     let self = new TagHeader()
     self.type = type
@@ -149,14 +226,13 @@ class TagHeader implements FromByteBuffer {
 }
 
 class ScriptData implements FromByteBuffer {
-  // private duration: Uint8Array
-  // private width: Uint8Array
-  // private height: Uint8Array
-  // private videodatarate: Uint8Array
-  // private framerate: Uint8Array
-  // private videocodecid: Uint8Array
-  // private data: Uint8Array
-  constructor() {}
+  metaInfo: Object
+  constructor() {
+    this.metaInfo = {}
+  }
+  formatPrint() {
+    console.log('script data  :', this.metaInfo)
+  }
   fromByteBuffer(buffer: ByteBuffer): this {
     throw new Error('Method not implemented.')
   }
@@ -164,15 +240,105 @@ class ScriptData implements FromByteBuffer {
     let buffer = Buffer.from(byteBuffer.toArrayBuffer())
     let onMetaData = fromAMF(buffer)
 
-    console.log('onMetaData', onMetaData)
+    let self = new ScriptData()
+    // console.log('onMetaData', onMetaData)
     let metaInfo = fromAMF(buffer.slice(13))
-    console.log('metaInfo', metaInfo)
-    return new ScriptData()
+    // console.log('metaInfo', metaInfo)
+    self.metaInfo = metaInfo as Object
+    return self
     // throw new Error('Method not implemented.')
   }
 }
-class VideoData {}
-class AudioData {}
+class VideoData implements FromByteBuffer {
+  frameType: FrameType
+  encodeType: EncodeType
+  data: ArrayBuffer
+  // acvPacketType 和 compositionTimeOffset 当且仅当为AVC类型才存在
+  // avcPacketType: AVCPacketType | null
+  // compositionTimeOffset: number
+
+  constructor() {
+    this.frameType = FrameType.KeyFrame
+    this.encodeType = EncodeType.AVC
+    // this.avcPacketType = null
+    this.data = new ArrayBuffer(0)
+    // this.compositionTimeOffset = 0
+  }
+  formatPrint() {
+    console.log('---------------------video---------------------------')
+    console.log('frame type     :', this.frameType)
+    console.log('encode type    :', this.encodeType)
+    // console.log('avc packet type:', this.avcPacketType)
+    // console.log('compos time    :', this.compositionTimeOffset)
+  }
+  fromByteBuffer(buffer: ByteBuffer): this {
+    throw new Error('Method not implemented.')
+  }
+  static fromByteBuffer(buffer: ByteBuffer): VideoData {
+    let videoArg = buffer.readByte()
+    let frameType = ((videoArg & 0b11110000) >> 4) as FrameType
+    let encodeType = (videoArg & 0b00001111) as EncodeType
+    // let packetType = null
+    // let compositionTimeOffset = 0
+    // if (encodeType === EncodeType.AVC) {
+    //   packetType = buffer.readByte() as AVCPacketType
+    //   let compositionTime = buffer.readBytes(3)
+    //   if (packetType == AVCPacketType.AVC_NALU) {
+    //     compositionTimeOffset =
+    //       (compositionTime.readInt16() << 8) + compositionTime.readInt8()
+    //   }
+    // }
+
+    let data = buffer.slice(buffer.offset, buffer.limit + 1).toArrayBuffer()
+    let self = new VideoData()
+    self.frameType = frameType
+    self.encodeType = encodeType
+    self.data = data
+    // self.avcPacketType = packetType
+    return self
+  }
+}
+class AudioData implements FromByteBuffer {
+  soundFormat: AudioSoundFormat
+  samplingRate: AudioSamplingRate
+  samplingPrecision: AudioSamplingPrecision
+  track: AudioSoundTrack
+  data: ArrayBuffer
+  constructor() {
+    this.soundFormat = AudioSoundFormat.AAC
+    this.samplingRate = AudioSamplingRate.SAMPLING_44_KHZ
+    this.samplingPrecision = AudioSamplingPrecision.SND_16_BIT
+    this.track = AudioSoundTrack.SND_STEREO
+    this.data = new ArrayBuffer(0)
+  }
+  formatPrint() {
+    console.log('------------------audio-------------------')
+    console.log('sound format      :', this.soundFormat)
+    console.log('sampling rate     :', this.samplingRate)
+    console.log('sampling precision:', this.samplingPrecision)
+    console.log('track             :', this.track)
+  }
+  fromByteBuffer(buffer: ByteBuffer): this {
+    throw new Error('Method not implemented.')
+  }
+  static fromByteBuffer(buffer: ByteBuffer): AudioData {
+    let audioArg = buffer.readByte()
+    let audioFormatType = ((audioArg & 0b11110000) >> 4) as AudioSoundFormat
+    let audioSamplingRate = ((audioArg & 0b00001100) >> 2) as AudioSamplingRate
+    let audioSamplePrecision = ((audioArg & 0b00000010) >>
+      1) as AudioSamplingPrecision
+    let audioTrack = (audioArg & 1) as AudioSoundTrack
+    let self = new AudioData()
+    self.soundFormat = audioFormatType
+    self.samplingRate = audioSamplingRate
+    self.samplingPrecision = audioSamplePrecision
+    self.track = audioTrack
+
+    let data = buffer.slice(buffer.offset, buffer.limit + 1).toArrayBuffer()
+    self.data = data
+    return self
+  }
+}
 
 class FlvDecoder implements FromByteBuffer {
   flvHeader: FlvHeader
@@ -189,7 +355,7 @@ class FlvDecoder implements FromByteBuffer {
     let header = FlvHeader.fromByteBuffer(buffer)
     header.formatPrint()
     while (true) {
-      let previousTagSize = buffer.readInt32()
+      let previousTagSize = buffer.readUint32()
       let tag = Tag.fromByteBuffer(buffer)
     }
     return self
